b2e778383fad3c66dcb3c410c57b46b4d98ec403
Artem Kokobelyan <artyomkokobelyanwork@gmail.com>
2024-10-05 17:37:56 +0400

    first commit

---
+++ src/bubbleSort.java
@@ -0,0 +1,21 @@
+public class bubbleSort {
+    class Solution {
+        public int[] sortArray(int[] nums) {
+
+            boolean swapped = true;
+            while (swapped) {
+                swapped = false;
+                for (int i =0; i < nums.length - 1; i ++) {
+                    if (nums[i] > nums[i + 1]) {
+                        int temp = nums[i];
+                        nums[i] = nums[i + 1];
+                        nums[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+            return nums;
+        }
+    }
+
+}
